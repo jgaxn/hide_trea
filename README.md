b854e6099012995a7e6f35a24f209053265f9f3f
jgaxn <jgaxn0@gmail.com>
2015-02-27 00:02:17 -0800

    v0.4 initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hide_trea
+A chrome extension to hide the name of Trea Turner
+
+Inspired by 
+http://www.gaslampball.com/2015/2/26/8114543/quantum-baseball-physics-what-happens-when-a-player-to-be-named-later
---
+++ hide_trea.js
@@ -0,0 +1,24 @@
+// Modify the content somehow...
+var doFilter = function(textNode) {
+    textNode.data = textNode.data.replace(/Trea Turner/g, 'XXXX XXXXXX');
+}
+
+// Create a MutationObserver to handle events
+// (e.g. filtering TextNode elements)
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.addedNodes) {
+            [].slice.call(mutation.addedNodes).forEach(function(node) {
+                if (node.nodeName.toLowerCase() == "#text") {
+                    doFilter(node);
+                }
+            });
+        }
+    });
+});
+
+// Start observing "childList" events in document and its descendants
+observer.observe(document, {
+    childList: true,
+    subtree:   true
+});
